f5d0cb69cb0acadd4f8629ca359431d789dd5c0f
Daniel <daniel.urena@habilisoft.do>
2024-08-17 17:39:22 -0400

    Init

--- src/main/java/org/habilisoft/zemi/user/command/DeleteRole.java
+++
@@ -1,8 +0,0 @@
-package org.habilisoft.zemi.user.command;
-
-import org.habilisoft.zemi.shared.Command;
-import org.habilisoft.zemi.user.domain.RoleName;
-
-import java.time.LocalDateTime;
-
-public record DeleteRole(RoleName roleName, String user, LocalDateTime time) implements Command { }
--- src/main/java/org/habilisoft/zemi/user/exception/InvalidOldPasswordException.java
+++
@@ -1,9 +0,0 @@
-package org.habilisoft.zemi.user.exception;
-
-import org.habilisoft.zemi.shared.DomainException;
-
-public final class InvalidOldPasswordException extends DomainException {
-    public InvalidOldPasswordException() {
-        super("Invalid old password");
-    }
-}
